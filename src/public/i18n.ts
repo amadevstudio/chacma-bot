f68e60c8b0b6cdc7ec13440b00d6c282eb1d2568
kinton <kintonk@yandex.ru>
2024-12-30 13:17:44 +0300

    Update dictionary

--- src/public/i18n.ts
+++ src/public/i18n.ts
@@ -3,35 +3,18 @@ import type { Dictionary } from "yau/src/i18n/setup";
 type AvailableLanguages = "ru";
 
 export default function configureI18n({
-                                        appName
-                                      }: {
+  appName,
+}: {
   appName: string;
 }): Dictionary<AvailableLanguages> {
   return {
     start: {
       s: {
-        asd: {},
         message: {
           // en: `Welcome to ${appName}! This is the /start answer`,
-          ru: `Добро пожаловать в ${appName}`
+          ru: `Добро пожаловать в ${appName}`,
         },
       },
-      n: {
-        zxc: {
-          numtest: {
-            singular: {
-              ru: "{0} облако, {1}"
-            },
-            few: {
-              ru: "{0} облака, {1}"
-            },
-            many: {
-              ru: "{0} облаков, {1}"
-            }
-          }
-        }
-      }
-    }
+    },
   };
 }
-

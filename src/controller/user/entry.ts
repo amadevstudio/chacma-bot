7440ce968820ef81d6032405d78411ac308040d6
kinton <kintonk@yandex.ru>
2025-01-10 22:50:18 +0300

    Some fixes

--- src/controller/user/entry.ts
+++ src/controller/user/entry.ts
@@ -2,6 +2,10 @@ import type { MessageStructure } from "yau/src/controller/types";
 import type { ConstructedParams } from "yau/src/core/types";
 import { localRouteNameMap } from "../routes";
 
+type MenuData = {
+  someData?: string;
+};
+
 export async function start(d: ConstructedParams) {
   const messages: MessageStructure[] = [
     {
@@ -9,31 +13,30 @@ export async function start(d: ConstructedParams) {
       text: d.i18n.t(["start", "s", "message"]),
       inlineMarkup: [
         [
-          {
-            text: "Open menu",
-            asd: "kek",
-            data: { $tp: localRouteNameMap.menu },
-          },
+          d.components.buildButton(localRouteNameMap.menu, "Open menu", {
+            someData: "Some data",
+          } as MenuData),
         ],
-        [d.components.buildButton(localRouteNameMap.menu, "Open menu")],
-        // [
-        //   d.components.buildButton(
-        //     localRouteNameMap.deepMethod,
-        //     "Or go deeper"
-        //   ),
-        // ],
+        [
+          d.components.buildButton(
+            localRouteNameMap.deepMethod,
+            "Or go deeper"
+          ),
+        ],
       ],
     },
-  ];
+  ] as const;
 
   return d.render(messages, { resending: d.isCommand });
 }
 
 export async function menu(d: ConstructedParams) {
+  const data = d.unitedData as MenuData;
+
   const messages: MessageStructure[] = [
     {
       type: "text",
-      text: "Hello there",
+      text: data.someData ? "Hello there with " + data.someData : "Hello there",
       inlineMarkup: [
         [d.components.buildButton(localRouteNameMap.deepMethod, "Deeper")],
       ],
@@ -43,7 +46,7 @@ export async function menu(d: ConstructedParams) {
       text: "And there",
       inlineMarkup: d.components.goBack.buildLayout(),
     },
-  ];
+  ] as const;
   return d.render(messages, { resending: d.isCommand });
 }
 
@@ -54,6 +57,6 @@ export async function deepMethod(d: ConstructedParams) {
       text: "Just goBack state",
       inlineMarkup: d.components.goBack.buildLayout(),
     },
-  ];
+  ] as const;
   return d.render(messages);
 }
